1fc3b8391b52c7c225ff937b577c178fbc1df16f
brightchul <57323359+brightchul@users.noreply.github.com>
2022-06-04 15:25:34 +0900

    feat: lecture1 과제 2, 3

--- lecture1/bin/main/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    val calcExample = CalcHomework();
-    print("-2 * -3 + 0.4 / - 0.2 = ")
-    println(calcExample.calc("-2 * -3 + 0.4 / - 0.2"))
-    print("-2 -3 + 0.4 = ")
-    println(calcExample.calc("-2 -3 + 0.4"))
-}
--- lecture1/src/main/kotlin/CalcHomework.kt
+++ lecture1/src/main/kotlin/CalcHomework.kt
@@ -1,32 +1,42 @@
-import java.util.Arrays
-
 class CalcHomework {
-
-    private val trimRegex: Regex = """[^.\d-+*/]""".toRegex()
-
-    private fun trim(v: String): String {
-        return v.replace(trimRegex, "")
-    }
-
+    private val trimRegex: Regex = """[^.\d-+*\(\)/]""".toRegex()
     private val splitRegex: Regex = """[\-]*[.\d]+|[\+\-\*\/\(\)]""".toRegex()
-
     private val getSingleMinusBeforeNumberRegex = """(?<=\d)\-(?=\d)""".toRegex()
+
+    private fun trim(v: String): String = v.replace(trimRegex, "")
 
     private fun replaceMinusToPlusMinus(v: String) = v.replace(getSingleMinusBeforeNumberRegex, "+-")
 
+    private fun operateBracket(operatorStack: MutableList<Symbol>, postfixNotationStack: MutableList<Element>, bracket: Bracket) {
+        when (bracket) {
+            is OpenBracket -> operatorStack.add(bracket)
+            is CloseBracket -> {
+                while (operatorStack.last() !is OpenBracket) {
+                    postfixNotationStack.add(operatorStack.removeLast())
+                }
+                operatorStack.removeLast()
+            }
+        }
+    }
 
+    // 후위표기식으로 변환합니다.
     private fun convertPostfix(v: String): MutableList<Element> {
-        val postfixNotationStack = mutableListOf<Element>()
-        val operatorStack = mutableListOf<Symbol>()
-        val splitSequence = splitRegex.findAll(v)
 
-        splitSequence.forEach { v ->
-            when (val symbol = getSymbolObject(v.value)) {
+        // 후위표기식 스택입니다. 반환되는 결과값입니다.
+        val postfixNotationStack = mutableListOf<Element>()
+
+        // 후위표기식으로 변환 과정중에 잠시 연산자를 저장하는 스택입니다.
+        val operatorStack = mutableListOf<Symbol>()
+
+        splitRegex.findAll(v).forEach { v ->
+            when (val symbol = getSymbolObject(v.value)) {      // 피연산자, 연산자, 괄호를 객체로 생성합니다.
                 is Operand -> postfixNotationStack.add(symbol)
                 is OperationCommand -> pushOperatorList(operatorStack, postfixNotationStack, symbol)
+                is Bracket -> operateBracket(operatorStack, postfixNotationStack, symbol)
             }
         }
 
+        // 마지막 남은 연산자들을 붙여줍니다.
         while (operatorStack.isNotEmpty()) {
             postfixNotationStack.add(operatorStack.removeLast())
         }
@@ -40,10 +50,13 @@ class CalcHomework {
             "-" -> Minus()
             "*" -> Multiply()
             "/" -> Divide()
+            "(" -> OpenBracket()
+            ")" -> CloseBracket()
             else -> Operand(value.toDouble())
         }
     }
 
+    // 해당 연산자를 연산자 우선순위에 맞춰서 후위표기식에 반영합니다.
     private fun pushOperatorList(operatorStack: MutableList<Symbol>, postfixNotationStack: MutableList<Element>, target: Symbol) {
         while (operatorStack.isNotEmpty()) {
             if (operatorStack.last() < target) {
@@ -55,9 +68,11 @@ class CalcHomework {
     }
 
     fun calc(v: String): Double {
+        // 후위표기식으로 먼저 변환
         val postfixList = convertPostfix(replaceMinusToPlusMinus(trim(v)))
         val operandStack = mutableListOf<Operand>()
 
+        // 후위 표기식으로 변환된 값을 연산
         while (postfixList.isNotEmpty()) {
             when (val element = postfixList.removeFirst()) {
                 is Operand -> operandStack.add(element)
@@ -70,7 +85,6 @@ class CalcHomework {
                 }
             }
         }
-
         return operandStack.removeLast().value
     }
 }
@@ -86,6 +100,12 @@ interface Symbol : Element, Comparable<Symbol> {
         return compareValuesBy(this, other) { it.priority }
     }
 }
+
+interface Bracket : Symbol
+
+class OpenBracket(override val priority: Int = 0) : Bracket
+
+class CloseBracket(override val priority: Int = 0) : Bracket
 
 interface OperationCommand : Symbol {
     override val priority: Int
